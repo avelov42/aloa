62ba7c10fd3a27e24151ebf264c399c4c23a90ff
Piotr Rybicki <loudrainbow.dev@gmail.com>
2016-09-09 21:30:06 +0200

    fresh libgdx + freetype project

---
+++ desktop/src/com/avelov/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.avelov.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.avelov.Aloa;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Aloa(), config);
+	}
+}
