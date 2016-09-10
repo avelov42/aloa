c528640614ff3d06999e930888ded76be8a6488b
Piotr Rybicki <avelov.mail@gmail.com>
2016-09-10 16:40:24 +0200

    removed unused imports

--- core/src/com/avelov/Aloa.java
+++ core/src/com/avelov/Aloa.java
@@ -1,11 +1,9 @@
 package com.avelov;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 /* General list
 @todo Choose production font and scale it properly (let it replace loaded font in skin)
